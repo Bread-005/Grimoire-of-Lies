b2c13d877d42b1adaf11001f4e165fcdaf0692ad
jenspromess <jenspromess@web.de>
2026-01-27 17:01:32 +0100

    added PlayerCount Selection

--- main.js
+++ main.js
@@ -1,25 +1,45 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const playerCountInput = document.getElementById("player-count-input");
 
-    const container = document.getElementById("grimoire");
+    createGrimoire();
 
-    const centerX = 300;
-    const centerY = 300;
-    const radius = 220;
+    if (!localStorage.getItem("player-count")) {
+        localStorage.setItem("player-count", "8");
+    }
+    playerCountInput.value = localStorage.getItem("player-count");
 
-    const playerCount = 12;
+    playerCountInput.addEventListener("input", () => {
+        if (playerCountInput.value > 12) {
+            playerCountInput.value = 12;
+        }
+        localStorage.setItem("player-count", playerCountInput.value);
+        createGrimoire();
+    });
 
-    for (let i = 0; i < playerCount; i++) {
-        const offset = -Math.PI / 2;
-        const angle = offset + (2 * Math.PI / playerCount) * i;
 
-        const x = centerX + radius * Math.cos(angle);
-        const y = centerY + radius * Math.sin(angle);
+    function createGrimoire() {
+        const grimoire = document.getElementById("grimoire");
+        grimoire.innerHTML = "";
 
-        const circle = document.createElement("div");
-        circle.classList.add("player-circle");
-        circle.style.left = x + "px";
-        circle.style.top = y + "px";
+        const centerX = 300;
+        const centerY = 300;
+        const radius = 220;
 
-        container.append(circle);
+        const playerCount = Number(localStorage.getItem("player-count"));
+
+        for (let i = 0; i < playerCount; i++) {
+            const offset = -Math.PI / 2;
+            const angle = offset + (2 * Math.PI / playerCount) * i;
+
+            const x = centerX + radius * Math.cos(angle);
+            const y = centerY + radius * Math.sin(angle);
+
+            const circle = document.createElement("div");
+            circle.classList.add("player-circle");
+            circle.style.left = x + "px";
+            circle.style.top = y + "px";
+
+            grimoire.append(circle);
+        }
     }
 });
